cac519513544aec0790804bc1d2e4346fbace956
Penelope Hill <penelope@squareup.com>
2015-08-17 17:14:39 -0700

    Yaml to mysql workflow is working properly

--- mysql_etl_tasks.py
+++ mysql_etl_tasks.py
@@ -5,12 +5,140 @@ read data from original, summarize and insert into summarized
 
 
 what's working: insert data, copy data to new table
-what's not working: taking summarized data and inserting into new table
+		taking summarized data and inserting into new table
+what's needed: use luigi to create task flow
 """
 
 
 import mysql.connector
+import luigi
+import mysql_target
 
+# class MysqlTarget(luigi.Target):
+#     """
+#     Target for a resource in local mysql
+#     """
+
+#     def __init__(self, mysql_connection, schema, table, key, value):
+
+#         self.connection = mysql_connection
+#         self.schema = schema
+#         self.table = table
+#         self.key = key
+#         self.value = value
+
+
+#     def create_marker_table(self):
+
+#         connection = self.connection.connect()
+#         cursor = connection.cursor()
+#         cursor.execute(
+#             """
+#                 CREATE TABLE IF NOT EXISTS markers (
+#                     id              BIGINT(20)    NOT NULL AUTO_INCREMENT
+#                     , schema_name   VARCHAR(128)  NOT NULL
+#                     , table_name    VARCHAR(128)  NOT NULL
+#                     , mark_key      VARCHAR(128)  NOT NULL
+#                     , mark_value    VARCHAR(128)  NOT NULL
+#                     , inserted      TIMESTAMP DEFAULT NOW()
+#                     , PRIMARY KEY (id)
+#                     , KEY idx_table_mark (schema_name, table_name, mark_key)
+#                 );
+#             """)
+#         connection.commit()
+#         connection.close()
+
+
+#     def exists(self):
+
+#         # create the marker table if it doesn't exist already
+#         self.create_marker_table()
+#         # connect to Unity
+#         connection = self.connection.connect()
+#         cursor = connection.cursor()
+#         # check that the key, value pair exists against the object of interest
+#         cursor.execute(
+#             """
+#                 SELECT 1
+#                 FROM markers
+#                 WHERE
+#                     schema_name = '{schema}'
+#                     AND table_name = '{table}'
+#                     AND mark_key = '{key}'
+#                     AND mark_value = '{value}';
+#             """.format(schema=self.schema,
+#                        table=self.table,
+#                        key=self.key,
+#                        value=self.value))
+#         row = cursor.fetchone()
+#         connection.close()
+#         return row is not None
+
+
+#     def mark_table(self):
+
+#         # create the table if it doesn't already
+#         self.create_marker_table()
+#         # connect to local mysql
+#         connection = self.connection.connect()
+#         cursor = connection.cursor()
+#         # insert a mark against the table
+#         cursor.execute(
+#             """
+#                 INSERT INTO markers (schema_name, table_name, mark_key, mark_value)
+#                 VALUES ('{schema}', '{table}', '{key}', '{value}');
+#             """.format(schema=self.schema,
+#                        table=self.table,
+#                        key=self.key,
+#                        value=self.value)
+#             )
+#         connection.commit()
+#         # close the connection
+#         connection.close()
+
+
+
+class FirstTask(luigi.Task):
+    """"""
+    def output(self):
+    	return MysqlTarget(mysql_connection, 
+    						
+    						'summaries', 
+    						'python_count', 
+    						str(datetime.utcnow().date()))
+    	database, user, password, table, update_id):
+        # return square_luigi.UnityTarget(unity
+        #                                 , 'batcave_production_proxy'
+        #                                 , 'payments'
+        #                                 , 'payments_today'
+        #                                 , str(datetime.utcnow().date()))
+
+    def run(self):
+		cnx = mysql.connector.connect(user='', password='',
+                              host='127.0.0.1',
+                              database='luigid')
+
+		cursor = cnx.cursor()
+        cursor.execute(
+            """
+            SELECT 1
+            FROM batcave_production_proxy.payments
+            WHERE created_at >= DATE(NOW())
+            LIMIT 1
+            """)
+        row = cursor.fetchone()
+        connection.close()
+
+        if row is None:
+            return
+        else:
+            mark = square_luigi.UnityTarget(unity
+                                        , 'batcave_production_proxy'
+                                        , 'payments'
+                                        , 'payments_today'
+                                        , str(datetime.utcnow().date()))
+            mark.mark_table()
+            return
 
 def create_data(): 
 	cnx = mysql.connector.connect(user='', password='',
@@ -90,8 +218,3 @@ def copy_data_to_diff_table(): #reads data from one table and writes some fields
 	cnx.close()
 
 # copy_data_to_diff_table()
-
-
-
-
-
---
+++ yaml_to_mysql.py
@@ -0,0 +1,48 @@
+import yaml
+import mysql.connector
+
+class YamlData():
+	def __init__(self, name, style, description, quantity):
+		self.name = name
+		self.style = style
+		self.description = description
+		self.quantity = quantity
+
+	def __repr__(self):
+		return "%s(name=%r, style=%r, description=%r, quantity=%r)" % (
+			self.__class__.__name__, self.name, self.style, self.description, self.quantity)
+
+cnx = mysql.connector.connect(user='', password='',
+                          host='127.0.0.1',
+                          database='yaml_practice')
+
+cursor = cnx.cursor()
+cursor.execute("""
+		CREATE TABLE IF NOT EXISTS children_stories (
+			name VARCHAR(20), 
+			style VARCHAR(20), 
+			description VARCHAR(20), 
+			quantity INT(10))
+			""")
+
+with open("hickory.yaml", 'r') as ymlfile:
+    data = yaml.load(ymlfile)
+    yd =YamlData(name=data['name'], style=data['style'], description=data['description'], quantity=data['quantity'])
+
+cursor.execute("""
+	INSERT INTO 
+	children_stories (name, style, description, quantity)
+	VALUES (%s, %s, %s, %s) 
+	""", (yd.name, yd.style, yd.description, yd.quantity))
+cnx.commit()
+cnx.close()
+
+
+
+
+
+
+
+
+
+
